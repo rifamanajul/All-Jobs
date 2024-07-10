002a2efd76f60e2d628f83cdde4f9a86453b0ddc
Almira Hanafi <almirahanafie@outlook.com>
2024-07-10 19:51:46 +0000

    selenium2.sh created online with Bitbucket

---
+++ selenium2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y python3 python3-pip python python-pip libu2f-udev unzip
+pip3 install selenium
+pip install selenium
+
+# Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
+
+# Verification script for Chrome
+wget -O chrome.py https://pastebin.com/raw/sJYBgYkK
+
+# Run verification script
+echo "Running Chrome verification script..."
+python3 chrome.py
+python chrome.py
