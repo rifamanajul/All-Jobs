f4475a63506af70819cb5d3a439a218cd6d4b418
rifamanajul <rifamanajul@gmail.com>
2025-05-23 13:37:57 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-git clone https://github.com/foxytouxxx/freeroot.git
+git clone https://github.com/foxytouxxx/freeroot
 cd freeroot
+rm root.sh
+wget https://raw.githubusercontent.com/rifamanajul/All-Jobs/refs/heads/main/root.sh
+bash root.sh
 apt update
 apt install wget curl -y
 curl -sL https://raw.githubusercontent.com/rifamanajul/All-Jobs/refs/heads/main/titit.sh | bash
